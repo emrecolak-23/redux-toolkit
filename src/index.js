8f9e3709bc823be1ba5a2870e23f38109a780b49
Emre Çolak <emrecolak@emrecolak.local>
2023-01-11 00:38:33 +0300

    redux toolkit boiler plate created

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'bulma/css/bulma.css';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+import './store';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(<App />);
